a586ed0ec6175714a4da4515a7c22cf6efc4bbd2
LewFortwangler <lfortwan@kent.edu>
2020-04-06 11:46:38 -0400

    Modifyed input program, added 9 more states data
    -Program will now read in all excel files from 2011-2017 without needing to restart program and manually type in new doc name.
    -Processed tax data for AK, AL, AR, AZ, CA, CO, CT, DE, FL

--- TaxDataRead/TaxDataRead/Program.cs
+++ TaxDataRead/TaxDataRead/Program.cs
@@ -11,55 +11,61 @@ namespace TaxDataRead
     {
         static void Main(string[] args)
         {
-            string filePath = @"C:\Users\Parox\Desktop\bigdata\TXtaxData\2017.xlsx";
-            string output = @"C:\Users\Parox\Documents\GitHub\BigDataAnalyticsZillow\ProcessedInput\TX\2017.txt";
-
-            if (File.Exists(filePath))
+            int docsProcessed = 0;
+            while(docsProcessed <= 6) // reads through and processes all tax data from 2011-2017 per state
             {
-                Console.WriteLine("File Found.");
+                string[] input = { "2011.xls", "2012.xls", "2013.xls", "2014.xls", "2015.xls", "2016.xls", "2017.xlsx" };
+                string[] output = { "2011.txt", "2012.txt", "2013.txt", "2014.txt", "2015.txt", "2016.txt", "2017.txt", };
+                string filePath = @"C:\Users\Parox\Desktop\bigdata\taxdata\FL\%inputFileName%".Replace("%inputFileName%", input[docsProcessed]);
+                string writePath = @"C:\Users\Parox\Documents\GitHub\BigDataAnalyticsZillow\ProcessedInput\FL\%outputFileName%".Replace("%outputFileName%", output[docsProcessed]);
+
+                if (File.Exists(filePath))
+                {
+                    Console.WriteLine(input[docsProcessed] + " Found.");
+                }
+                else
+                {
+                    Console.WriteLine("Error. " + input[docsProcessed] + " not found.");
+                }
+
+                // testing the class
+                Excel excel = new Excel(filePath, 1);
+
+                //creating a list of strings to store processed zipcode data
+                List<string> allZipcodes = new List<string>();
+                int x = 14;
+                int y = 0;
+
+                while (excel.readCell(x, y) != 99999)
+                {
+                    //Console.WriteLine("Reading...");
+                    ZipCodeData entry = new ZipCodeData();
+                    entry.zipCodeID = excel.readCell(x, y).ToString();
+
+                    y += 2; // moves over 2 columns (gets total # of return for zipcode)
+                    entry.totalReturns = (int)excel.readCell(x, y);
+
+                    x += 5; // moves down 5 rows (gets returns filed above $100k)
+                    entry.returnsAbove100k = (int)excel.readCell(x, y);
+
+                    x += 1; // moves down 1 row (gets returns filed above $200k)
+                    entry.returnsAbove200k = (int)excel.readCell(x, y);
+
+                    // adds processed info per zipcode to output file
+                    allZipcodes.Add(entry.zipCodeID + "," +
+                                    entry.totalReturns.ToString() + "," +
+                                    entry.returnsAbove100k.ToString() + "," +
+                                    entry.returnsAbove200k.ToString());
+
+                    // moves down 2 rows and resets to first colomn to get next zipcode
+                    y = 0;
+                    x += 2;
+                    Console.WriteLine(x); // Debug line, prints row of excel spreadsheet to locate errors
+                }
+                File.WriteAllLines(writePath, allZipcodes);
+                Console.WriteLine("Finished " + input[docsProcessed] + ".");
+                ++docsProcessed;
             }
-            else
-            {
-                Console.WriteLine("Error. File not found.");
-            }
-
-            // testing the class
-            Excel excel = new Excel(filePath, 1);
-
-            //starting to assemble a method to reading the important parts of the tax data
-            //List<ZipCodeData> taxData = new List<ZipCodeData>();
-            List<string> allZipcodes = new List<string>();
-            int x = 14;
-            int y = 0;
-
-            while (excel.readCell(x, y) != 99999)
-            {
-                //Console.WriteLine("Reading...");
-                ZipCodeData entry = new ZipCodeData();
-                entry.zipCodeID = excel.readCell(x, y).ToString();
-
-                y += 2; // moves over 2 columns
-                entry.totalReturns = (int)excel.readCell(x, y);
-
-                x += 5;
-                entry.returnsAbove100k = (int)excel.readCell(x, y);
-
-                x += 1;
-                entry.returnsAbove200k = (int)excel.readCell(x, y);
-
-                allZipcodes.Add(entry.zipCodeID + "," +
-                                entry.totalReturns.ToString() + "," +
-                                entry.returnsAbove100k.ToString() + "," +
-                                entry.returnsAbove200k.ToString());
-
-                // moves down 2 rows and resets to first colomn to get next zipcode
-                y = 0;
-                x += 2;
-                Console.WriteLine(x);
-            }
-            File.WriteAllLines(output, allZipcodes);
-            Console.WriteLine("Finished.");
-            Console.ReadLine(); // pause
         }
     }
 }
